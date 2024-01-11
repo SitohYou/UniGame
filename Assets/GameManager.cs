56813324076206c7a7564a56437e0848e430fb8f
SitohYou <himuronbotti@yahoo.co.jp>
2024-01-11 15:56:15 +0900

    UniGame like Suica

---
+++ Assets/GameManager.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance { get; private set; }
+    public bool isNext { get; set; }
+    public int MaxSeedNo { get;private set; }
+
+    [SerializeField] private seed[] seedPrefab;
+    [SerializeField] private Transform seedPosition;
+    [SerializeField] private Text txtScore;
+
+    private int totalscore;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instance = this;
+        isNext = false;
+        MaxSeedNo = seedPrefab.Length;
+        totalscore = 0;
+        SetScore(totalscore);
+        CreateSeed();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isNext)
+        {
+            isNext = false;
+            Invoke("CreateSeed", 2f);
+        }
+    }
+
+    private void CreateSeed()
+    {
+        int i = Random.Range(0, MaxSeedNo - 3);
+        seed seedIns = Instantiate(seedPrefab[i], seedPosition);
+        seedIns.seedNo = i;
+        seedIns.gameObject.SetActive(true);
+    }
+    public void MergeNext(Vector3 target, int seedNo)
+    {
+        seed seedIns = Instantiate(seedPrefab[seedNo + 1], target, Quaternion.identity, seedPosition);
+        seedIns.seedNo = seedNo + 1;
+        seedIns.isDrop = true;
+        seedIns.GetComponent<Rigidbody2D>().simulated = true;
+        seedIns.gameObject.SetActive(true);
+        totalscore += (int)Mathf.Pow(3, seedNo);
+        SetScore(totalscore);
+    }
+
+    private void SetScore(int score)
+    {
+        txtScore.text = score.ToString();
+    }
+}
---
+++ Assets/seed.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class seed : MonoBehaviour
+{
+    private Rigidbody2D _rb;
+
+    public bool isMergeFlag = false;
+    public bool isDrop = false;
+    public int seedNo;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0) && isDrop == false)
+        {
+            Drop();
+        }
+        if (isDrop) return;
+
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        float halfWidth = this.transform.localScale.x * 0.5f;
+
+        mousePos.x = Mathf.Clamp(mousePos.x, -3.45f + halfWidth, 3.45f - halfWidth);
+        mousePos.y = 3.5f;
+
+        transform.position = mousePos;
+    }
+
+    private void Drop()
+    {
+        isDrop = true;
+        _rb.simulated = true;
+        GameManager.Instance.isNext = true;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        GameObject colobj = collision.gameObject;
+
+        if (colobj.CompareTag("seed"))
+        {
+            seed colseed = collision.gameObject.GetComponent<seed>();
+            if (seedNo == colseed.seedNo &&
+                !isMergeFlag &&
+                !colseed.isMergeFlag &&
+                seedNo < GameManager.Instance.MaxSeedNo - 1) 
+            {
+                isMergeFlag = true;
+                colseed.isMergeFlag = true;
+                GameManager.Instance.MergeNext(transform.position, seedNo);
+                Destroy(gameObject);
+                Destroy(colseed.gameObject);
+            }
+        }
+    }
+}
